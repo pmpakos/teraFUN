30dc97764e915560df5c0db97e7a7666e86fed86
Nick Fotos <nickfotosgr@gmail.com>
2018-01-28 19:05:59 +0200

    Fix checks in Search

--- gradle/test_project/src/main/webapp/js/search.js
+++ gradle/test_project/src/main/webapp/js/search.js
@@ -44,14 +44,13 @@ $(document).on('click','.clear-button',function(){
 
 $(document).on('blur','.distance',function(event){
 	var content = $(this).val();
-	invalidArray = content.match(/^[-a-zA-Zα-ωΑ-Ω]/g);
+	invalidArray = content.match(/[-a-zA-Zα-ωΑ-Ω]/g);
 	
 	if(invalidArray != null){
 		valid_distance = 1;
 		alert("Μη έγκυρη απόσταση!");
 		$('#distance').val('');
 	}
-
 	else if(content>70){
 		valid_distance=1;
 		alert("H μέγιστη δυνατή απόσταση είναι 70 χλμ.!");
@@ -64,8 +63,11 @@ $(document).on('blur','.distance',function(event){
 
 $(document).on('blur','.age',function(event){
 	var content = $(this).val();
-	invalidArray = content.match(/^[-a-zA-Zα-ωΑ-Ω]/g);
 	
+	invalidArray = content.match(/[-a-zA-Zα-ωΑ-Ω]/g);
+	
+
+
 	if(invalidArray != null){
 		valid_age = 1;
 		alert("Μη έγκυρη ηλικία!");
@@ -90,10 +92,44 @@ $(document).on('click','.search-button',function(){
 	if($('#c4').is(":checked")){
 		education=1;
 	}
+
+	var text_search = document.getElementById("text_search").value;
+	console.log("Search: "+text_search);
 	
-	console.log("Out: "+outdoor+" In: "+indoor+" Fun: "+fun+" Edu: "+education);
+	var date = document.getElementById("inputID").value;
+	console.log("Date: "+date);
+	
+	var e = document.getElementById("sel1");
+	var ticket = e.options[e.selectedIndex].value;
+	console.log("No. Tickets: "+ticket);
 
-	if(valid_age==1 || valid_distance==1){
 
+	var age = document.getElementById("age").value;
+	console.log("Age: "+age);
+
+
+	var distance = document.getElementById("distance").value;
+	console.log("Distance: "+distance);
+
+
+	console.log(" Out: "+outdoor+" In: "+indoor+" Fun: "+fun+" Edu: "+education);
+
+	empty_in1 = (text_search.length>0)? 0: 1;
+	empty_in2 = (date.length>0)? 0: 1;
+	empty_in4 = (age.length>0)? 0: 1;
+	empty_in5 = (distance.length>0)? 0: 1;
+
+	if(empty_in1*empty_in2*empty_in4*empty_in5 > 0){
+		alert("Συμπληρώστε πεδίο προς αναζήτηση.");
+	}
+	else if (valid_age==1){
+		
+		alert("Μη έγκυρη εισαγωγή πεδίων.");
+	}
+	else if(valid_distance ==1 ){
+		alert("Μη έγκυρη εισαγωγή πεδίων.");
+	}
+	else{
+		alert("ΘΑ ΤΟ ΨΑΞΩ ΣΕ ΛΙΓΟ ΑΣΕ ΜΕ!!!");
 	}
 });
